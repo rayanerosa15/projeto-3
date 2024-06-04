b7f76bd63b210a6ccda4a38d3024e4a3424d588b
rayanerosa15 <134058761+rayanerosa15@users.noreply.github.com>
2024-06-04 11:40:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -38,7 +38,19 @@ const perguntas = [
 ]
 let atual = 0;
 let perguntaAtual;
+
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
+
