0f23771d826c68026beeeb9d03f0d2bea0d71b4a
rayanerosa15 <134058761+rayanerosa15@users.noreply.github.com>
2024-06-04 16:51:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
 const perguntas = [
   
     {
@@ -41,15 +42,8 @@ let perguntaAtual;
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    mostraAlternativas();
-}
-function mostraAlternativas() {
-    for(const alternativa of perguntaAtual.alternativas) {
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativa.textContent = alternativa;
-        caixaAlternativas.appendChild(botaoAlternativas);
-    }
+    caixaPerguntas.textContent = perguntaAtual.enunciado
+  
 }
 
 mostraPergunta();
