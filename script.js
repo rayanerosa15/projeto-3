9f28ae81e5b3fdc131b9a6787674199f004a42d8
rayanerosa15 <134058761+rayanerosa15@users.noreply.github.com>
2024-07-02 22:33:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,51 +5,97 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
+    {
+        enunciado: "Hoje é dia de levar seu petshop, você prefere ir de manhã ou a tarde?",
+        alternativas: [
+            {
+                texto: "manhã!",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "tarde!,
+                afirmacao: "afirmacao"
+            }
+        ]
+    },
+    {
+        enunciado: "Qual banho você prefere? banho e tosa tradicional ou apenas higiênico?",
+        alternativas: [
+            {
+                texto: "tradicional",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "apenas higiênico",
+                afirmacao: "afirmacao"
+            }
+        ]
+    },
+    {
+        enunciado: "deseja algum acessório?",
+        alternativas: [
+            {
+                texto: "sim, obrigado!",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "não, obrigado!",
+                afirmacao: "afirmacao"
+            }
+          
+        ]
+      
+},
+    {
+        enunciado: "Ao final do procedimento podemos tirar foto?",
+        alternativas: [
+            {
+                texto: " simm!",
+                afirmacao: "afirmacao"
+            },
+            {
+                texto: "simm!",
+                afirmacao: "afirmacao"
+            }
+          
+        ]    
+}, 
   
-    {
-        enunciado: "Hoje é dia e levar seu cachorro ao petshop, você prefere ir e manhã ou a tarde?",
-        alternativas: [
-            "manhã",
-            "tarde"
-        ]
-    },
-    {
-        enunciado: "Qual banho você prefere?,banho e tosa tradicional ou banho higiênico?",
-        alternativas: [
-            "banho e tosa tradicional.",
-            "banho higiênico."
-        ]
-    },
-    {
-        enunciado: "Deseja algum acessório?",
-        alternativas: [
-            "Não,obrigada!",
-            "Sim,obrigada!."
-        ]
-    },
-    {
-        enunciado: "Ao final do procedimento podemos tirar foto!",
-        alternativas: [
-            "simm!.",
-            "Não!."
-        ]
-    },
-    
 ];
+
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
-function mostraPergunta(){
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+     caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
+
 function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
+    for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
-
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historia += afirmacoes + "";
+    atual++;
+    mostraPergunta();
+}
+   
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
 mostraPergunta();
